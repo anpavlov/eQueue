6871082d6c08bbadc1126363b68e8d1df809b8c6
nanomishka <mihail777str@yandex.ru>
2016-02-15 23:16:21 +0000

    mysql start

--- equeue/settings.py
+++ equeue/settings.py
@@ -77,11 +77,12 @@ WSGI_APPLICATION = 'equeue.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '/etc/mysql/ejueue.cnf',
+        },
     }
 }
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
