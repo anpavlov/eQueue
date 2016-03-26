ebd2a5b6e38febdd14e8b6685b5969dcef260ba3
Alex <digwnews@gmail.com>
2016-03-26 16:03:17 +0300

    ticket view done

--- app/src/main/java/com/example/alex/inqueue/QueueActivity.java
+++ app/src/main/java/com/example/alex/inqueue/QueueActivity.java
@@ -2,12 +2,20 @@ package com.example.alex.inqueue;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
-public class QueueActivity extends Activity {
+public class QueueActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_queue);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("Название очереди");
+        }
+
     }
 }
