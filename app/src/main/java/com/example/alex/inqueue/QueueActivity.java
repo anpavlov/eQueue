1a0f6f15df8238209206db9dc7970faa5e50805b
Alex <digwnews@gmail.com>
2016-03-26 18:56:48 +0300

    enter queue done

--- app/src/main/java/com/example/alex/inqueue/QueueActivity.java
+++ app/src/main/java/com/example/alex/inqueue/QueueActivity.java
@@ -1,9 +1,13 @@
 package com.example.alex.inqueue;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 
 public class QueueActivity extends AppCompatActivity {
 
@@ -16,6 +20,24 @@ public class QueueActivity extends AppCompatActivity {
         if (getSupportActionBar() != null) {
             getSupportActionBar().setTitle("Название очереди");
         }
+
+        final Button enterButton = (Button) findViewById(R.id.enter);
+        if(enterButton != null) {
+            enterButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    enterButton.setVisibility(View.GONE);
+                    Animation bottomUp = AnimationUtils.loadAnimation(QueueActivity.this,
+                            R.anim.bottom_up);
+                    ViewGroup hiddenPanel = (ViewGroup) findViewById(R.id.ticket);
+                    if (hiddenPanel != null) {
+                        hiddenPanel.startAnimation(bottomUp);
+                        hiddenPanel.setVisibility(View.VISIBLE);
+                    }
+                }
+            });
+        }
+
 
     }
 }
