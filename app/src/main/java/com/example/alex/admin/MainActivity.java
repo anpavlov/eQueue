d757296f0a834d059adab3ed1041fd2312736e37
Alex <digwnews@gmail.com>
2016-03-27 15:53:40 +0300

    admin design init

---
+++ app/src/main/java/com/example/alex/admin/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.alex.admin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("Ваши очереди");
+        }
+    }
+}
