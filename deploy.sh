41f6e4c3c0ed3b4c7f42bb41da39b14b6ce02fef
Alex <digwnews@gmail.com>
2016-04-08 00:05:25 +0300

    travis conf

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,3 @@ kill $(ps aux | grep 'tarantool' | awk '{print $2}') >& /dev/null
 nohup ~/tarantool/tarantool/src/tarantool ~/eQueue/equeue.lua > Output.out 2> Error.err < /dev/null &
 kill $(ps aux | grep 'main.py runserver' | awk '{print $2}') >& /dev/null
 nohup python eQueue/main.py runserver -p 30212 > serv.out 2> Error.err < /dev/null &
-echo 0
