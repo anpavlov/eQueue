9b2741748eca01f37f552f727b085c6503dd56c8
Andrey Pavlov <dir94@mail.ru>
2016-04-25 13:17:02 +0300

    one client = many qids

--- websocket.py
+++ websocket.py
@@ -9,7 +9,7 @@ class SocketHandler(websocket.WebSocketHandler):
 
     def __init__(self, application, request, **kwargs):
         super(SocketHandler, self).__init__(application, request, **kwargs)
-        self.qid = -1
+        self.qids = []
 
     def check_origin(self, origin):
         return True
@@ -20,52 +20,45 @@ class SocketHandler(websocket.WebSocketHandler):
         #     clients.append(self)
 
     def on_message(self, message):
-        if self.qid != -1:
-            return
         try:
             qid = int(message)
-            self.qid = qid
-            if qid not in clients or not isinstance(clients[qid], list):
-                clients[qid] = []
+            if qid not in self.qids:
+                self.qids.append(qid)
+                if qid not in clients or not isinstance(clients[qid], list):
+                    clients[qid] = []
 
-            if self not in clients[qid]:
-                clients[qid].append(self)
+                if self not in clients[qid]:
+                    clients[qid].append(self)
 
         except ValueError:
             return
 
     def on_close(self):
-        if self.qid != -1 and self in clients[self.qid]:
-            clients[self.qid].remove(self)
+        for qid in self.qids:
+            if self in clients[qid]:
+                clients[qid].remove(self)
 
 
 class ApiHandler(web.RequestHandler):
 
     @web.asynchronous
-    def get(self, *args):
+    def post(self):
         self.finish()
-        # try:
         qid = self.get_argument("qid")
         qid = int(qid)
-        action_type = self.get_argument("type")
-        action = self.get_argument("action")
-        # except web.MissingArgumentError:
-        #     raise web.HTTPError(400)
+        in_front = self.get_argument("in_front")
+        users_quantity = self.get_argument("users_quantity")
+        wait_time = self.get_argument("wait_time")
 
-        # if action_type != "all" "infront" "time"
-
-        data = {"qid": qid, "type": action_type, "action": action}
+        data = {"qid": qid, "in_front": in_front, "users_quantity": users_quantity, "wait_time": wait_time}
         j = json.dumps(data)
         if qid in clients and isinstance(clients[qid], list):
             for c in clients[qid]:
                 c.write_message(j)
 
 app = web.Application([
-    # (r'/', IndexHandler),
     (r'/ws/client/queue', SocketHandler),
     (r'/post/', ApiHandler),
-    # (r'/(favicon.ico)', web.StaticFileHandler, {'path': '../'}),
-    # (r'/(rest_api_example.png)', web.StaticFileHandler, {'path': './'}),
 ])
 
 if __name__ == '__main__':
