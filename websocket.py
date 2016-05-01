0dcdd90b2f9c99fb6baf6eac84aefe9744eb41a5
Andrey Pavlov <dir94@mail.ru>
2016-05-01 14:22:25 +0300

    changes :)

--- websocket.py
+++ websocket.py
@@ -1,4 +1,6 @@
 from tornado import websocket, web, ioloop
+from tornado.tcpserver import TCPServer
+from tornado.netutil import bind_unix_socket
 import json
 
 # clients[<queue_id>] = []
@@ -39,28 +41,58 @@ class SocketHandler(websocket.WebSocketHandler):
                 clients[qid].remove(self)
 
 
-class ApiHandler(web.RequestHandler):
+# class ApiHandler(web.RequestHandler):
+#
+#     @web.asynchronous
+#     def post(self):
+#         self.finish()
+#         qid = self.get_argument("qid")
+#         qid = int(qid)
+#         in_front = self.get_argument("in_front")
+#         users_quantity = self.get_argument("users_quantity")
+#         wait_time = self.get_argument("wait_time")
+#
+#         data = {"qid": qid, "in_front": in_front, "users_quantity": users_quantity, "wait_time": wait_time}
+#         j = json.dumps(data)
+#         if qid in clients and isinstance(clients[qid], list):
+#             for c in clients[qid]:
+#                 c.write_message(j)
 
-    @web.asynchronous
-    def post(self):
-        self.finish()
-        qid = self.get_argument("qid")
-        qid = int(qid)
-        in_front = self.get_argument("in_front")
-        users_quantity = self.get_argument("users_quantity")
-        wait_time = self.get_argument("wait_time")
 
-        data = {"qid": qid, "in_front": in_front, "users_quantity": users_quantity, "wait_time": wait_time}
-        j = json.dumps(data)
-        if qid in clients and isinstance(clients[qid], list):
-            for c in clients[qid]:
-                c.write_message(j)
+def read_callback(data_bytes):
+    print "received '" + data_bytes + "'"
+    data = json.loads(data_bytes)
+    qid = int(data['qid'])
+    if qid in clients and isinstance(clients[qid], list):
+        for c in clients[qid]:
+            c.write_message(data_bytes)
+
+
+class EchoServer(TCPServer):
+
+    def handle_stream(self, stream, address):
+        stream.read_bytes(1000, streaming_callback=read_callback)
+
+        # self._stream = stream
+        # self._read_line()
+
+    # def _read_line(self):
+    #     self._stream.read_until('\n', self._handle_read)
+    #
+    # def _handle_read(self, data):
+    #     self._stream.write(data)
+    #     self._read_line()
+
 
 app = web.Application([
     (r'/ws/client/queue', SocketHandler),
-    (r'/post/', ApiHandler),
+    # (r'/post/', ApiHandler),
 ])
 
 if __name__ == '__main__':
+    server = EchoServer()
+    unix_socket = bind_unix_socket('/tmp/foo.sock')
+    server.add_socket(unix_socket)
+    # server.listen(8889)
     app.listen(8888)
     ioloop.IOLoop.instance().start()
