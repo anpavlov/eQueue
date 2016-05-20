87bda55223b3659d3de36e313bd6f9b012e903d1
Alex <digwnews@gmail.com>
2016-05-20 11:08:48 +0300

    Update responses.py

--- apis/responses.py
+++ apis/responses.py
@@ -1,8 +1,8 @@
-BAD_REQUEST = {'code': 400, 'body': {'error': 'invalid request params'}}
-INVALID_TOKEN = {'code': 403, 'body': {'error': 'invalid token'}}
-QUEUE_NOT_FOUND = {'code': 404, 'body': {'error': 'queue was not found'}}
-ALREADY_IN_QUEUE = {'code': 400, 'body': {'error': 'you have been already in this queue'}}
-EMPTY_QUEUE = {'code': 400, 'body': {'error': 'empty queue'}}
-EMAIL_BUSY = {'code': 403, 'body': {'error': 'email is busy'}}
-ACCESS_DENIED = {'code': 403, 'body': {'error': 'wrong pair email/password'}}
-UNDEFINED_USER = {'code': 200, 'body': {'error': 'undefined user'}}
+BAD_REQUEST = {'code': 400, 'body': {'error': 'Неверные параметры для запроса'}}
+INVALID_TOKEN = {'code': 403, 'body': {'error': 'Ваша сессия устарела'}}
+QUEUE_NOT_FOUND = {'code': 404, 'body': {'error': 'Очередь не найдена'}}
+ALREADY_IN_QUEUE = {'code': 400, 'body': {'error': 'Вы уже стоите в этой очереди'}}
+EMPTY_QUEUE = {'code': 400, 'body': {'error': 'В очереди нет ни одного человека'}}
+EMAIL_BUSY = {'code': 403, 'body': {'error': 'E-mail уже зарегистрирован'}}
+ACCESS_DENIED = {'code': 403, 'body': {'error': 'Неверная пара email/password'}}
+UNDEFINED_USER = {'code': 200, 'body': {'error': 'Неопределенный пользователь'}}
