2d6a959fbd3122733969287a8d84fde710cc975c
Alex <digwnews@gmail.com>
2016-09-11 18:03:31 +0000

    fix server config

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ cd ~/eQueue/
 git pull >& /dev/null
 cd ..
 kill $(ps aux | grep 'tarantool' | awk '{print $2}') >& /dev/null
-nohup ~/tarantool/tarantool/src/tarantool ~/eQueue/equeue.lua > tarantool.out 2> tarantool.out < /dev/null &
+nohup ~/tarantool/tarantool/src/tarantool ~/eQueue/taran/equeue.lua > tarantool.out 2> tarantool.out < /dev/null &
 sleep 1
 kill $(ps aux | grep 'main.py runserver' | awk '{print $2}') >& /dev/null
 nohup python eQueue/main.py runserver -p 30212 > flask.out 2> flask.out < /dev/null &
